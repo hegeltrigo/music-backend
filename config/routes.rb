1c62d6a343c915ccfeff4ac8c06ea373897849c2
Hegel Trigo <hegeltrigo@gmail.com>
2018-07-11 17:43:41 -0400

    updated how to get token

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   resources :letters
   devise_for :users,
-             path: '',
-             path_names: {
-               sign_in: 'login',
-               sign_out: 'logout',
-               registration: 'signup'
-             },
              controllers: {
                sessions: 'sessions',
                registrations: 'registrations'
