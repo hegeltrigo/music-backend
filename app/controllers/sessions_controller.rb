f390c2065ae8546fadcfa9598e38ba9fc2ab74fa
Hegel Trigo <hegeltrigo@gmail.com>
2018-07-11 22:26:05 -0400

    added cors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,13 @@
 class SessionsController < Devise::SessionsController
   # respond_to :json
 
+  def create
+     self.resource = warden.authenticate!(auth_options)
+     sign_in(resource_name, resource)
+     yield resource if block_given?
+     json_response(resource.attributes.merge(token: current_token))
+  end
+
   private
 
   def respond_with(resource, _opts = {})
@@ -10,5 +17,9 @@ class SessionsController < Devise::SessionsController
 
   def respond_to_on_destroy
     head :no_content
+  end
+
+  def current_token
+    request.env['warden-jwt_auth.token']
   end
 end
